350f2d2436ecb26e66c196f8198bf97af9c5ef37
varsha11796 <74728835+varsha11796@users.noreply.github.com>
2020-11-21 13:56:04 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,33 @@
+public class Vowel
+{
+	public static void main(String []args)
+	{
+		char ch='a';
+		switch(ch)
+		{
+			case 'a' : System.out.println("character  "+ch+"\tis Vowel");
+				break;
+			case 'e' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'i' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'o' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'u' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'A' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'E' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'I' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'O' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			case 'U' :System.out.println("character\t"+ch+"\tis Vowel");
+				break;
+			default :System.out.println("Character is  Consonant");
+				break;
+		}
+		
+	}
+}
