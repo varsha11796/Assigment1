4dc035a03b35242c1ee212588bed4aae02ef3a54
varsha <varshapatil11796@gmail.com>
2020-11-21 13:52:53 +0530

    numbers

---
+++ core1/src/core1/NumbersWhile.java
@@ -0,0 +1,17 @@
+
+
+//print 1-10 using do while loop
+package core1;
+
+public class NumbersWhile
+{
+	public static void main(String []args)
+	{
+		int i=1;
+		do{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+			
+	}	
+}
